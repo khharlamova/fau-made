49c73eef3722ba8f49f9956145bc52cb43f3915c
Maria Kharlamova <57595885+khharlamova@users.noreply.github.com>
2024-06-05 12:04:30 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+output_dir=~/project
+processed_file_1="$output_dir/processed_co2_emissions_2018_2022.csv"
+processed_file_2="$output_dir/processed_air_quality_2018_2022.csv"
+
+python3 pipeline.py
+
+# Check if the output files are created
+if [[ -f "$processed_file_1" ]]; then
+    echo "Test passed: $processed_file_1 exists."
+else
+    echo "Test failed: $processed_file_1 does not exist."
+    exit 1
+fi
+
+if [[ -f "$processed_file_2" ]]; then
+    echo "Test passed: $processed_file_2 exists."
+else
+    echo "Test failed: $processed_file_2 does not exist."
+    exit 1
+fi
+
+echo "All tests passed."
